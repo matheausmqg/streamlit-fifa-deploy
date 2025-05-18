f3584a71c31bf66cbff66183402777a9fb85cf70
matheausmqg <matheaus.bdk@gmail.com>
2025-05-18 17:27:53 -0300

    adding link button

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -5,19 +5,22 @@ from datetime import datetime
 import openai
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv(
+        "datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
     df_data = df_data[df_data["Value(£)"] > 0]
     df_data = df_data.sort_values(by="Overall", ascending=False)
     st.session_state["data"] = df_data
 
 st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
-st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
+st.sidebar.markdown(
+    "Desenvolvido por [Matheaus Marques, no curso da ASIMOV](https://asimov.academy)")
 
 
-btn = st.button("Acesse os dados no Kaggle")
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+btn = st.link_button(
+    "Acesse os dados no Kaggle",
+    "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+)
 
 st.markdown(
     """
